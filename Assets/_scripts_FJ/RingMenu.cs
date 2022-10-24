387965022528f6a90ad21ec5470748f2a8cd82d7
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-24 13:13:17 +0200

    commit 24 10 2022 13 12
    scriptable objects armas y menu radial de selección de arma

---
+++ Assets/_scripts_FJ/RingMenu.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RingMenu : MonoBehaviour
+{
+    public WeaponsData[] weapons;
+    public RingPiece ringPiecePrefab;
+
+
+    private RingPiece[] ringPieces;
+    private float degreesPerPiece;
+    private float gapDegrees = 1f;
+    private void Start()
+    {
+        degreesPerPiece = 360f / weapons.Length;
+        float distanceToIcon = Vector3.Distance(ringPiecePrefab.icon.transform.position, ringPiecePrefab.backGround.transform.position);
+
+        ringPieces = new RingPiece[weapons.Length];
+
+        for(int i = 0; i < weapons.Length; i++ )
+        {
+            ringPieces[i] = Instantiate(ringPiecePrefab, transform);
+
+            ringPieces[i].backGround.fillAmount = (1f / weapons.Length) - (gapDegrees / 360f);
+            ringPieces[i].backGround.transform.localRotation = Quaternion.Euler(0, 0, degreesPerPiece / 2f + gapDegrees / 2f + i * degreesPerPiece);
+
+            ringPieces[i].icon.sprite = weapons[i].icon.sprite;
+
+            Vector3 directionVector = Quaternion.AngleAxis(i * degreesPerPiece, Vector3.forward) * Vector3.up;
+            Vector3 movementVector = directionVector * distanceToIcon;
+            ringPieces[i].icon.transform.localPosition = ringPieces[i].backGround.transform.localPosition + movementVector;
+        }
+    }
+    private void Update()
+    {
+        int activeElement = GetActiveElement();
+        HighLightActiveElement(activeElement);
+        RespondToMouseInput();
+    }
+
+    private void RespondToMouseInput()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+
+        }
+    }
+
+    private int GetActiveElement()
+    {
+        Vector3 screenCenter = new Vector3(Screen.width / 2, Screen.height / 2);
+        Vector3 cursorVector = Input.mousePosition - screenCenter;
+
+        float mouseAngle = Vector3.SignedAngle(Vector3.up, cursorVector, Vector3.forward) + degreesPerPiece / 2f;
+        float normalizedMouseAngle = NormalizeAngle(mouseAngle);
+        return (int)(normalizedMouseAngle / degreesPerPiece);
+    }
+
+
+    private void HighLightActiveElement(int activeElement)
+    {
+        for(int i = 0; i < ringPieces.Length; i++)
+        {
+            if(i==activeElement)
+            {
+                ringPieces[i].backGround.color = new Color(1f, 1f, 0.75f);
+            }
+            else
+            {
+                ringPieces[i].backGround.color = new Color(1f, 1f, 0.5f);
+            }
+        }
+    }
+    private float NormalizeAngle(float a) => (a + 360f) % 360f;
+    
+}
---
+++ Assets/_scripts_FJ/WeaponsData.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "Weapons", menuName ="JAM/WeaponsData",order = 1)]
+public class WeaponsData : ScriptableObject
+{
+    public string weaponName;
+    public GameObject weaponPrefab;
+    public int weaponDamage;
+    public bool leftHanded;
+    public bool throwable;
+    public Image icon;
+}
