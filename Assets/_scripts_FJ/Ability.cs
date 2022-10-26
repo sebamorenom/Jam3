c033bc7e54ac045c74ef0505cb6ecbd93c49c481
fjmm01 <francisco.murillo@escuelaestech.es>
2022-10-26 19:38:40 +0200

    commit

--- Assets/_scripts_FJ/Ability.cs
+++ Assets/_scripts_FJ/Ability.cs
@@ -2,17 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ability : MonoBehaviour
+public class Ability : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public string name;
+    public float coolDown;
+    public float activeTime;
 
-    // Update is called once per frame
-    void Update()
+
+    public virtual void Activate()
     {
-        
+
     }
 }
