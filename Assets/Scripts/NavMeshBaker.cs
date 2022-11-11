b51323719c74e24b0db85ff4922d667bc48c7026
Sebastian Moreno <sebastian.moreno@escuelaestech.es>
2022-11-11 21:26:56 +0100

    Spawners added

--- Assets/Scripts/NavMeshBaker.cs
+++ Assets/Scripts/NavMeshBaker.cs
@@ -2,12 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class NavMeshBaker : MonoBehaviour
 {
     // Start is called before the first frame update
     public NavMeshSurface navSurface;
-    void Start()
+    void OnStart()
     {
         navSurface.BuildNavMesh();
     }
